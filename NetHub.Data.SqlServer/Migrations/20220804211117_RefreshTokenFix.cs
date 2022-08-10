1f028e92c5fe897a95249295b8b75465da5d4cf6
tweeker <geektweeker@gmail.com>
2022-08-10 17:38:00 +0300

    RefreshToken migrations

---
+++ NetHub.Data.SqlServer/Migrations/20220804211117_RefreshTokenFix.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetHub.Data.SqlServer.Migrations
+{
+    public partial class RefreshTokenFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_UserProfiles_UserId1",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserTokens_UserId1",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "ConcurrencyStamp",
+                value: "fcccfd13-43da-4fdf-8a0d-ed758708ae75");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "ConcurrencyStamp",
+                value: "ec34a456-28ac-44c9-bf8a-da3fee692cdf");
+
+            migrationBuilder.UpdateData(
+                table: "UserProfiles",
+                keyColumn: "Id",
+                keyValue: 19L,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "Registered", "SecurityStamp" },
+                values: new object[] { "c2846cc6-520d-44f8-9acf-4d30e8ace71c", "AQAAAAEAACcQAAAAEI9Ulz1AkdJc7Wn8/ojA7Hj3DIensl6sYZJ+493cuvKyLTCtcTp50smNp3Ca+iNDyw==", new DateTime(2022, 8, 4, 21, 11, 16, 523, DateTimeKind.Utc).AddTicks(3508), "6d98a04c-ec10-4473-ac47-bee827c407fe" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "UserId1",
+                table: "AspNetUserTokens",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "ConcurrencyStamp",
+                value: "86c12cb2-4c33-452d-acc6-0d0e49828d1b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "ConcurrencyStamp",
+                value: "5e3b6b55-ecc4-4a0f-be82-42b794337ff1");
+
+            migrationBuilder.UpdateData(
+                table: "UserProfiles",
+                keyColumn: "Id",
+                keyValue: 19L,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "Registered", "SecurityStamp" },
+                values: new object[] { "9b2d3e39-30ca-4e90-ad3f-bc3caf383f8d", "AQAAAAEAACcQAAAAEAP0oQkr4ZuVSxHxvhOqOCPMXnrq9mdxIdRnQSmefJtN0Sd7fmlP5EFRdStxwUeGCg==", new DateTime(2022, 7, 17, 19, 21, 46, 457, DateTimeKind.Utc).AddTicks(4269), "0b4df101-8163-4d3b-b05d-d1db82f4a8be" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserTokens_UserId1",
+                table: "AspNetUserTokens",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_UserProfiles_UserId1",
+                table: "AspNetUserTokens",
+                column: "UserId1",
+                principalTable: "UserProfiles",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ NetHub.Data.SqlServer/Migrations/20220806145059_NotNullHtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetHub.Data.SqlServer.Migrations
+{
+    public partial class NotNullHtml : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Html",
+                table: "ArticleLocalizations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "ConcurrencyStamp",
+                value: "ebdc302d-729f-416d-850d-1f595536307d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "ConcurrencyStamp",
+                value: "0a8f6d3e-56c5-4cab-a1d1-097b2ee61aa0");
+
+            migrationBuilder.UpdateData(
+                table: "UserProfiles",
+                keyColumn: "Id",
+                keyValue: 19L,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "Registered", "SecurityStamp" },
+                values: new object[] { "c0d0564f-8d86-43df-a9d5-6ec408b61db4", "AQAAAAEAACcQAAAAECIr/NR7j+tSnhmRlE5bdubeplwmOuo2SUndtUkPcikEBNAwnvn3fZHDjatsuaZXfg==", new DateTime(2022, 8, 6, 14, 50, 58, 325, DateTimeKind.Utc).AddTicks(8858), "fbfe86b5-cacd-4b2f-814f-92265d37d71a" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Html",
+                table: "ArticleLocalizations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "ConcurrencyStamp",
+                value: "fcccfd13-43da-4fdf-8a0d-ed758708ae75");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "ConcurrencyStamp",
+                value: "ec34a456-28ac-44c9-bf8a-da3fee692cdf");
+
+            migrationBuilder.UpdateData(
+                table: "UserProfiles",
+                keyColumn: "Id",
+                keyValue: 19L,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "Registered", "SecurityStamp" },
+                values: new object[] { "c2846cc6-520d-44f8-9acf-4d30e8ace71c", "AQAAAAEAACcQAAAAEI9Ulz1AkdJc7Wn8/ojA7Hj3DIensl6sYZJ+493cuvKyLTCtcTp50smNp3Ca+iNDyw==", new DateTime(2022, 8, 4, 21, 11, 16, 523, DateTimeKind.Utc).AddTicks(3508), "6d98a04c-ec10-4473-ac47-bee827c407fe" });
+        }
+    }
+}
